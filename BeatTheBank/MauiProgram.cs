00a45d15b3828ca942d328e8e5d37f4c535157af
Allan Ritchie <allan.ritchie@gmail.com>
2026-03-17 19:04:26 -0400

    Add Sentry

--- BeatTheBank/MauiProgram.cs
+++ BeatTheBank/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 using CommunityToolkit.Maui;
+using Microsoft.Extensions.Configuration;
 using Plugin.Maui.Audio;
 using Shiny.SqliteDocumentDb;
 
@@ -14,18 +15,24 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        var builder = MauiApp
-            .CreateBuilder()
+        var builder = MauiApp.CreateBuilder();
+        builder
             .UseMauiApp<App>()
             .UseMauiCommunityToolkit()
             .AddAudio()
             .UseShinyShell(x => x.AddGeneratedMaps())
+            .UseSentry(x => x.Dsn = builder.Configuration["SentryDsn"]!)
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        builder.Configuration.AddJsonStream(
+            typeof(MauiProgram)
+                .Assembly
+                .GetManifestResourceStream("BeatTheBank.appsettings.json")!
+        );
         builder.AddShinyMediator(x => x
             .AddMediatorRegistry()
             .UseMaui()
